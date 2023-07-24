c9fe476b6269612f1aafdb36215a66f8717e1e5d
Paul Hoger <paul.hoger@student.kit.edu>
2023-07-24 22:52:49 +0200

    Add column annotation

---
+++ src/main/java/de/paul2708/worm/columns/Column.java
@@ -0,0 +1,10 @@
+package de.paul2708.worm.columns;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+
+    String value();
+}
