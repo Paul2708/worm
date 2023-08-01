b52a776b09fea9966f5748a2054e268a61b2b5fc
Paul Hoger <paul.hoger@student.kit.edu>
2023-08-01 22:15:38 +0200

    Implement MySQL unit test

---
+++ src/test/java/de/paul2708/worm/example/database/MySQLDatabaseTest.java
@@ -0,0 +1,32 @@
+package de.paul2708.worm.example.database;
+
+import de.paul2708.worm.database.Database;
+import de.paul2708.worm.database.sql.MySQLDatabase;
+import org.junit.jupiter.api.Assertions;
+
+import java.sql.*;
+
+public class MySQLDatabaseTest extends DatabaseTest {
+
+    private static final String HOST = "host.docker.internal";
+    private static final int PORT = 3306;
+    private static final String DATABASE = "worm";
+    private static final String USERNAME = "worm";
+    private static final String PASSWORD = "worm_password";
+
+    @Override
+    public Database createEmptyDatabase() {
+        // Drop table
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://%s:%s/%s".formatted(HOST, PORT, DATABASE), USERNAME, PASSWORD);
+            Statement statement = connection.createStatement();
+            statement.execute("DROP TABLE persons");
+        } catch (SQLException e) {
+            Assertions.fail("Failed to clear tables", e);
+        }
+
+        // Create database
+        return new MySQLDatabase(HOST, PORT, DATABASE, USERNAME, PASSWORD);
+    }
+}
