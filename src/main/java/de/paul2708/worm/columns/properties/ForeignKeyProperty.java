9caa8b7aed6e591c2a68d9cdff87626022bbb3a3
Paul Hoger <paul.hoger@student.kit.edu>
2023-08-13 10:01:54 +0200

    Save the foreign entity before storing the target entity

--- src/main/java/de/paul2708/worm/columns/properties/ForeignKeyProperty.java
+++ src/main/java/de/paul2708/worm/columns/properties/ForeignKeyProperty.java
@@ -1,14 +1,33 @@
 package de.paul2708.worm.columns.properties;
 
+import de.paul2708.worm.columns.AttributeResolver;
+import de.paul2708.worm.columns.ColumnAttribute;
+
 public class ForeignKeyProperty implements ColumnProperty {
 
     private final Class<?> foreignKey;
 
+    private final AttributeResolver resolver;
+
     public ForeignKeyProperty(Class<?> foreignKey) {
         this.foreignKey = foreignKey;
+
+        this.resolver = new AttributeResolver(foreignKey);
     }
 
     public Class<?> getForeignKey() {
         return foreignKey;
+    }
+
+    public String getForeignTable() {
+        return resolver.getTable();
+    }
+
+    public ColumnAttribute getForeignPrimaryKey() {
+        return resolver.getPrimaryKey();
+    }
+
+    public Object getForeignPrimaryKeyValue(Object entity) {
+        return resolver.getPrimaryKey().getValue(new AttributeResolver(entity.getClass()).getPrimaryKey().getValue(entity));
     }
 }
