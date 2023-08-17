a3f27e2a0462f7f81fd7ed9dbba208cca6a6fd1a
Tim Wöhrle <theserum.dev@gmail.com>
2023-08-17 15:56:58 +0200

    updated byte array impl

--- src/main/java/de/paul2708/worm/columns/properties/LengthRestrictedProperty.java
+++ src/main/java/de/paul2708/worm/columns/properties/LengthRestrictedProperty.java
@@ -1,16 +1,14 @@
 package de.paul2708.worm.columns.properties;
 
+import java.lang.reflect.Array;
+
 public record LengthRestrictedProperty(int length) implements ColumnProperty {
 
     public boolean isTooLong(Object value) {
         if (value instanceof String text) {
             return text.length() > length;
-        }
-        if (value instanceof Byte[] bytes) {
-            return bytes.length > length;
-        }
-        if (value instanceof byte[] bytes) {
-            return bytes.length > length;
+        } else if (value.getClass().isArray()) {
+            return Array.getLength(value) > length;
         }
         return false;
     }
