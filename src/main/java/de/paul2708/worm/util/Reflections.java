5f762c3bfaf62c54b31ce94fa3962e52c9fdf47f
Paul Hoger <paul.hoger@student.kit.edu>
2023-08-22 19:45:05 +0200

    Implement utility to check whether a class implements a specific interface

--- src/main/java/de/paul2708/worm/util/Reflections.java
+++ src/main/java/de/paul2708/worm/util/Reflections.java
@@ -2,6 +2,8 @@ package de.paul2708.worm.util;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Set;
 
 public class Reflections {
 
@@ -9,6 +11,31 @@ public class Reflections {
         ParameterizedType parameterizedType = (ParameterizedType) listField.getGenericType();
 
         return (Class<?>) parameterizedType.getActualTypeArguments()[0];
+    }
+
+    public static boolean isList(Class<?> listImplClass) {
+        return hasInterface(listImplClass, List.class);
+    }
+
+    public static boolean isSet(Class<?> setImplClass) {
+        return hasInterface(setImplClass, Set.class);
+    }
+
+    private static boolean hasInterface(Class<?> implClass, Class<?> interfaceClass) {
+        if (implClass == null) {
+            return false;
+        }
+        if (implClass.equals(interfaceClass)) {
+            return true;
+        }
+
+        for (Class<?> anInterface : implClass.getInterfaces()) {
+            if (anInterface.equals(interfaceClass)) {
+                return true;
+            }
+        }
+
+        return hasInterface(implClass.getSuperclass(), interfaceClass);
     }
 
     public static Field getField(Class<?> clazz, String field) {
--- src/test/java/de/paul2708/worm/util/ReflectionsTest.java
+++ src/test/java/de/paul2708/worm/util/ReflectionsTest.java
@@ -3,10 +3,9 @@ package de.paul2708.worm.util;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReflectionsTest {
 
@@ -14,11 +13,31 @@ public class ReflectionsTest {
     private Set<String> strings;
 
     @Test
-    void test() {
+    void testListElementTypes() {
         Field intsField = Reflections.getField(ReflectionsTest.class, "ints");
         Field stringsField = Reflections.getField(ReflectionsTest.class, "strings");
 
         assertEquals(Integer.class, Reflections.getElementType(intsField));
         assertEquals(String.class, Reflections.getElementType(stringsField));
+    }
+
+    @Test
+    void testListClass() {
+        assertTrue(Reflections.isList(List.class));
+        assertTrue(Reflections.isList(ArrayList.class));
+        assertTrue(Reflections.isList(LinkedList.class));
+
+        assertFalse(Reflections.isList(Set.class));
+        assertFalse(Reflections.isList(HashSet.class));
+    }
+
+    @Test
+    void testSetClass() {
+        assertTrue(Reflections.isSet(Set.class));
+        assertTrue(Reflections.isSet(HashSet.class));
+        assertTrue(Reflections.isSet(TreeSet.class));
+
+        assertFalse(Reflections.isSet(List.class));
+        assertFalse(Reflections.isSet(ArrayList.class));
     }
 }
