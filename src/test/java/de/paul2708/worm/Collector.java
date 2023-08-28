c28320e949195dfbacff2b70404e8de78ada644f
Paul Hoger <paul.hoger@student.kit.edu>
2023-08-28 14:11:51 +0200

    Fix wrong test implementation

--- src/test/java/de/paul2708/worm/Collector.java
+++ src/test/java/de/paul2708/worm/Collector.java
@@ -43,7 +43,7 @@ public class Collector {
     }
 
     public void removePrime(int prime) {
-        this.primeNumbers.remove(prime);
+        this.primeNumbers.removeIf(value -> value == prime);
     }
 
     public Set<String> getBadges() {
